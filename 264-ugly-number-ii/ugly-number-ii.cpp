efe850103ebc84ecf2895d6c054df8727094bd98
Suraj85297 <98026260+Suraj85297@users.noreply.github.com>
2024-08-18 18:14:30 +0530

    Time: 3 ms (76.61%) | Memory: 9.7 MB (56.48%) - LeetSync

---
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -0,0 +1,30 @@
+
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        std::vector<int> uglyNumbers(n);
+        uglyNumbers[0] = 1;
+        int index2 = 0, index3 = 0, index5 = 0;
+        int nextMultipleOf2 = 2, nextMultipleOf3 = 3, nextMultipleOf5 = 5;
+        
+        for (int i = 1; i < n; i++) {
+            int nextUglyNumber = std::min({nextMultipleOf2, nextMultipleOf3, nextMultipleOf5});
+            uglyNumbers[i] = nextUglyNumber;
+            
+            if (nextUglyNumber == nextMultipleOf2) {
+                index2++;
+                nextMultipleOf2 = uglyNumbers[index2] * 2;
+            }
+            if (nextUglyNumber == nextMultipleOf3) {
+                index3++;
+                nextMultipleOf3 = uglyNumbers[index3] * 3;
+            }
+            if (nextUglyNumber == nextMultipleOf5) {
+                index5++;
+                nextMultipleOf5 = uglyNumbers[index5] * 5;
+            }
+        }
+        
+        return uglyNumbers[n-1];
+    }
+};
