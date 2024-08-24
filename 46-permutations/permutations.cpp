d48a9bb43e32a20d9a54372e36acc168fec3e317
Suraj85297 <98026260+Suraj85297@users.noreply.github.com>
2024-08-24 18:03:27 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (76.37%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+    void fn(int index, vector <int> &nums, vector <vector <int>> &ans){
+        if(index==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=index; i<nums.size(); i++){
+            swap(nums[index],nums[i]);
+            fn(index+1,nums,ans);
+            swap(nums[index],nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector <vector <int>> ans;
+        fn(0,nums,ans);
+        return ans;
+    }
+};
+/*
+class Solution {
+public:
     void fn(vector <vector <int>> &ans, vector <int> &ds, vector <int> &nums, vector <int> &freq){
         if(ds.size()==nums.size()){
             ans.push_back(ds);
@@ -24,3 +44,4 @@ public:
         return ans;
     }
 };
+*/
