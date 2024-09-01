634f41430f99545720c720f6d69b1b90b26b7729
Suraj85297 <98026260+Suraj85297@users.noreply.github.com>
2024-09-01 15:55:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (43.74%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void fn (int ind, vector <int> &nums, vector <vector<int>> &ans, vector <int> ds){
+            ans.push_back(ds);
+            for(int i=ind; i<nums.size(); i++){
+                if(i!=ind && nums[i]==nums[i-1]) continue;
+                ds.push_back(nums[i]);
+                fn(i+1,nums,ans,ds);
+                ds.pop_back();
+            }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector <vector <int>> ans;
+        vector <int> ds;
+        sort(nums.begin(),nums.end());
+        fn(0, nums, ans, ds);
+        return ans;
+    }
+};
