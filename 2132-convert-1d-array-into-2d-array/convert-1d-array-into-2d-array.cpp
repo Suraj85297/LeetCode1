792bd2e006eabfdd874e24ca572317f916c6e702
Suraj85297 <98026260+Suraj85297@users.noreply.github.com>
2024-09-01 14:04:27 +0530

    Time: 94 ms (15.95%) | Memory: 88 MB (73.42%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
         if(original.size()!=m*n){
@@ -15,5 +15,23 @@ public:
             ind+=n;
         }
         return result;        
+    }
+};*/
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        // Check if the total number of elements matches
+        if (original.size() != m * n) {
+            return {};
+        }
+
+        vector<vector<int>> result(m, vector<int>(n));
+
+        // Fill the 2D array by mapping elements from the original array
+        for (int i = 0; i < original.size(); i++) {
+            result[i / n][i % n] = original[i];
+        }
+
+        return result;
     }
 };
