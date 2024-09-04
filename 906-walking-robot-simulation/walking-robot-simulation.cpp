f8d0ac18ec9ac4ed23fe69deab39ed20d6c30f02
Suraj85297 <98026260+Suraj85297@users.noreply.github.com>
2024-09-04 21:44:42 +0530

    Time: 121 ms (25.41%) | Memory: 39.5 MB (65.75%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.cpp
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -1,47 +1,43 @@
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        unordered_map<string,int>mp;
-        for(auto it:obstacles){
-            string key = to_string(it[0])+"+"+to_string(it[1]);
-            mp[key]++;
+        unordered_set<string> st;
+        for(vector<int>& obs : obstacles) {
+            string key = to_string(obs[0]) + "_" + to_string(obs[1]);
+            st.insert(key);
         }
-        vector<vector<int>>direc = {{-1,0},{0,1},{1,0},{0,-1}}; 
-        // according to order W N E S
 
-        int dir = 1; // i.e. north 
-        // 0: west, 1: north, 2: east, 3: south
-        int x=0,y=0;
-        int mxDist=0;
-        for(int it:commands){
-            if(it==-2){
-                // move left
-                if(dir==0) dir = 3;
-                else dir--;
-            }
-            else if(it==-1){
-                // move right
-                dir = (dir+1)%4;
-            }
-            else{
-                int k = it;
-                for(int i=1; i<=k; i++){
-                    int newX = x+direc[dir][0];
-                    int newY = y+direc[dir][1];
-                    
-                    string key = to_string(newX)+"+"+to_string(newY);
-                    if(mp.find(key)!=mp.end()){
-                        // obstacle has been found so don't go there
+        int x = 0;
+        int y = 0;
+        int maxD = 0;
+
+        //Pointing to North
+        pair<int, int> dir = {0, 1}; //N
+
+        for(int i = 0; i < commands.size(); i++) {
+            if(commands[i] == -2) { //left 90 degree
+                dir = {-dir.second, dir.first};
+            } else if(commands[i] == -1) { //right 90 degree
+                dir = {dir.second, -dir.first};
+            } else { //move to the direction step by step
+                for(int step = 0; step < commands[i]; step++) {
+                    int newX = x + dir.first;
+                    int newY = y + dir.second;
+
+                    string nextKey = to_string(newX) + "_" + to_string(newY);
+
+                    if(st.find(nextKey) != st.end()) {
                         break;
                     }
-                    x = newX, y=newY;
-                    mxDist = max(mxDist, x*x+y*y);
+
+                    x = newX;
+                    y = newY;
                 }
             }
-        } 
 
-        return mxDist;
+            maxD = max(maxD, x*x + y*y);
+        }
 
-
+        return maxD;
     }
 };
