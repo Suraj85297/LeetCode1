48c6b58215fd6d5ce6f15de192b748b2a413a24e
Suraj85297 <98026260+Suraj85297@users.noreply.github.com>
2024-09-05 14:14:29 +0530

    Time: 14 ms (48.87%) | Memory: 8 MB (71.88%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
-    bool isValid(vector <vector<char>> &board, int row, int col, char c){
+    bool isValidChar(vector <vector<char>> &board, char c, int row, int col){
         for(int i=0; i<9; i++){
-            if(board[i][col]==c){
-                return false;
-            }
-            if(board[row][i]==c){
-                return false;
-            }
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c){
-                return false;
-            }
+            if(board[row][i]==c) return false;
+            if(board[i][col]==c) return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c) return false;
         }
         return true;
     }
@@ -19,7 +13,7 @@ public:
             for(int j=0; j<board[0].size(); j++){
                 if(board[i][j]=='.'){
                     for(char c='1'; c<='9'; c++){
-                        if(isValid(board, i, j, c)){
+                        if(isValidChar(board,c,i,j)){
                             board[i][j]=c;
                             if(solve(board)==true){
                                 return true;
@@ -35,7 +29,8 @@ public:
         }
         return true;
     }
-    void solveSudoku(vector<vector<char>>& board) {
-        solve (board);
+    void solveSudoku(vector<vector<char>>& board){
+        solve(board);
     }
+
 };
