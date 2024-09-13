950a4c03c5cc092a2d875a1e4910f7e71b607ea9
Suraj85297 <98026260+Suraj85297@users.noreply.github.com>
2024-09-13 13:18:08 +0530

    Time: 50 ms (88.01%) | Memory: 42.1 MB (53.29%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         vector <int> ans;
@@ -13,6 +13,35 @@ public:
             ans.push_back(xorElement);
             j++;
         }
+        return ans;
+    }
+};*/
+
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        int n = arr.size();
+        vector<int> prefixXor(n);
+        prefixXor[0] = arr[0];
+        
+        // Compute prefix XOR for the array
+        for (int i = 1; i < n; ++i) {
+            prefixXor[i] = prefixXor[i - 1] ^ arr[i];
+        }
+        
+        vector<int> ans;
+        
+        // Answer each query using the prefix XOR
+        for (const auto& query : queries) {
+            int left = query[0];
+            int right = query[1];
+            if (left == 0) {
+                ans.push_back(prefixXor[right]);
+            } else {
+                ans.push_back(prefixXor[right] ^ prefixXor[left - 1]);
+            }
+        }
+        
         return ans;
     }
 };
