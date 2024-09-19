554111856d6f44c4dfc6825f86a4728b54aff69f
Suraj85297 <98026260+Suraj85297@users.noreply.github.com>
2024-09-19 20:49:31 +0530

    Time: 7 ms (18.72%) | Memory: 13.8 MB (32.27%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -1,44 +1,32 @@
 class Solution {
 public:
-    // function to get the result of the operation
-    int perform(int x, int y, char op) {
-        if(op == '+') return x + y;
-        if(op == '-') return x - y;
-        if(op == '*') return x * y;
-        return 0;
-    }
-    
-    vector<int> diffWaysToCompute(string exp) {
-        
-        vector<int> results;
-        bool isNumber = 1;
-    
-        for(int i = 0; i < exp.length(); i++) {
-            // check if current character is an operator
-            if(!isdigit(exp[i])) {
-                
-                // if current character is not a digit then
-                // exp is not purely a number
-                isNumber = 0;
-                
-                // list of first operands
-                vector<int> left = diffWaysToCompute(exp.substr(0, i));
-                
-                // list of second operands
-                vector<int> right = diffWaysToCompute(exp.substr(i + 1));
-                
-                // performing operations
-                for(int x : left) {
-                    for(int y : right) {
-                        int val = perform(x, y, exp[i]);
-                        results.push_back(val);
+    vector <int> solve(string s){
+        vector <int> result;
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='+' || s[i]=='*' || s[i]=='-'){
+                vector <int> left_exp = solve(s.substr(0,i));
+                vector <int> right_exp = solve(s.substr(i+1));
+                 for(int x : left_exp){
+                    for(int y : right_exp){
+                        if(s[i]=='+'){
+                            result.push_back(x+y);
+                        }
+                        if(s[i]=='-'){
+                            result.push_back(x-y);
+                        }
+                        if(s[i]=='*'){
+                            result.push_back(x*y);
+                        }
                     }
-                }
-                
+                 }
             }
         }
-        
-        if(isNumber == 1) results.push_back(stoi(exp));
-        return results;
+        if(result.empty()){
+            result.push_back(stoi(s));
+        }
+        return result;
+    }
+    vector<int> diffWaysToCompute(string expression) {
+        return solve(expression);
     }
 };
